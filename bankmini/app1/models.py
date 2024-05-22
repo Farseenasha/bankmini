d86c5c2d7e59a621312e6675c80a9cf259648852
Farseenasha <farseenasha313@gmail.com>
2024-05-22 11:58:07 +0530

    first commit

---
+++ bankmini/app1/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class Customs(AbstractUser):
+    account_no = models.IntegerField(null=True,blank=True)
+    bank = models.CharField(max_length=50)
+    branch = models.CharField(max_length=50)
+    ifsc_code = models.IntegerField(null=True,blank=True)
+    pincode = models.IntegerField(null=True,blank=True)
+    acnt_no = models.IntegerField(null=True,blank=True)
+    amount = models.IntegerField(null=True,blank=True)
+    phone_no = models.IntegerField(null=True,blank=True)
+    address = models.CharField(max_length=50)
+    adhar_no = models.IntegerField(null=True,blank=True)
+    pancard_no = models.IntegerField(null=True,blank=True)
+    image = models.FileField()
+    dob = models.CharField(max_length=50)
+    user_type = models.CharField(max_length=50)
+
+class Transaction(models.Model):
+    user_id = models.IntegerField()
+    details = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    user_amount = models.IntegerField()
